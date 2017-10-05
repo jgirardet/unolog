e98498ff1e085b3c52d23496791096b309fbeac2
Jimmy Girardet <ijkl@tuta.io>
2017-10-05 22:54:32 +0200

    ok

--- patients/tests/conftest.py
+++ patients/tests/conftest.py
@@ -35,15 +35,3 @@ def patient_dict():
     p.__dict__.pop('_state')
     p.__dict__.pop('id')
     return p.__dict__
-
-
-@pytest.fixture(autouse=True, scope='function')
-def observation_nodb(patient_dict):
-    """
-    fixture for observation instance
-    """
-    p = Patient.objects.create(**patient_dict)
-    o = Mixer(commit=False).blend(
-        Observation,
-        patient=p, )
-    return o
