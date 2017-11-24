868744c5617ecb6d0f51c1742dd6fc4776b3b81b
Jimmy Girardet <test@ma.fr>
2017-11-24 14:55:12 +0100

    inhertit serializers

--- actes/serializers.py
+++ actes/serializers.py
@@ -1,14 +1,40 @@
-from rest_framework import serializers
-
 from actes.models import Observation
+from patients.models import Patient
+from rest_framework import serializers
+from unousers.models import UnoUser
 
 
-class ObservationSerializer(serializers.HyperlinkedModelSerializer):
+class ActeSerializer(serializers.HyperlinkedModelSerializer):
+    """
+    BaseActe serializer
+    """
+    url = serializers.HyperlinkedIdentityField(view_name='observation-detail')
+    pk = serializers.IntegerField(label='ID', read_only=True)
+    patient = serializers.HyperlinkedRelatedField(
+        queryset=Patient.objects.all(),
+        view_name='patient-detail',
+    )
+    created = serializers.DateTimeField(read_only=True)
+    modified = serializers.DateTimeField(read_only=True)
+    owner = serializers.HyperlinkedRelatedField(
+        queryset=UnoUser.objects.all(), view_name='unouser-detail')
+
+    class Meta:
+        fields = (
+            'url',
+            'pk',
+            'patient',
+            'created',
+            'modified',
+            'owner',
+        )
+
+
+class ObservationSerializer(ActeSerializer):
     """
     Observation serializer
     """
 
-    class Meta:
+    class Meta(ActeSerializer.Meta):
         model = Observation
-        fields = ('url', 'pk', 'patient', 'created', 'modified', 'owner',
-                  'motif', 'body')
+        fields = ActeSerializer.Meta.fields + ('motif', 'body')
