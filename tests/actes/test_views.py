b6c66911541882ace677f96127decd886c19ce85
Jimmy Girardet <ijkl@tuta.io>
2017-12-01 23:36:43 +0100

    add owner readonly

---
+++ tests/actes/test_views.py
@@ -0,0 +1,20 @@
+import pytest
+from mixer.backend.django import mixer
+from rest_framework.reverse import reverse, reverse_lazy
+from rest_framework.test import APIRequestFactory
+
+
+class TestObservationViewSet:
+
+    def test_owner_readonly(self, apiclient, patient):
+        id  = apiclient.handler._force_user.id
+        data = {
+        "patient": "http://localhost:8000/patients/1/",
+        "owner": "http://localhost:8000/users/"+str(id+1)+"/",
+        "motif": "cccccccccc",
+        "body": "cccccccc"
+        }
+        r = apiclient.post(
+            reverse('observation-list'), data=data, format='json')
+
+        assert  r.data['owner'][-2] == str(id), "owner should be logged user"
--- unolog/actes/views.py
+++ unolog/actes/views.py
@@ -1,17 +1,28 @@
-from rest_framework import viewsets
+from rest_framework import mixins, viewsets
 
 from .models import Observation
 from .permissions import OnlyOwnerCanEdit
 from .serializers import ObservationSerializer
 
-
 # Create your views here.
 
 
-class ObservationViewSet(viewsets.ModelViewSet):
+class ActeViewSet(viewsets.ModelViewSet):
+    """
+    base common viewset for ActeS
+    """
+    permission_classes = (OnlyOwnerCanEdit, )
+
+    def perform_create(self, serializer):
+        """
+        add user to owner saving instance
+        """
+        serializer.save(owner=self.request.user)
+
+
+class ObservationViewSet(ActeViewSet):
     """
     base viewset for patients
     """
     queryset = Observation.objects.all()
     serializer_class = ObservationSerializer
-    permission_classes = (OnlyOwnerCanEdit, )
