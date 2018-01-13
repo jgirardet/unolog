1b522cab4ce72297e934f86d85db4d036df98254
Jimmy Girardet <ijkl@tuta.io>
2018-01-14 00:23:04 +0100

    refactore env

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "unolog.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
@@ -18,4 +18,6 @@ if __name__ == "__main__":
                 "available on your PYTHONPATH environment variable? Did you "
                 "forget to activate a virtual environment?")
         raise
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(os.path.join(current_path, 'unolog'))
     execute_from_command_line(sys.argv)
