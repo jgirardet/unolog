a7a60b5322bb39bf9236fde8477eea446f9a4aef
Jimmy Girardet <ijkl@tuta.io>
2018-01-14 21:02:41 +0100

    pytest

--- launch.py
+++ launch.py
@@ -4,17 +4,17 @@ import os
 import sys
 
 if sys.argv[-1] == 'rs':
-    os.system('pipenv run python ./unolog/manage.py runserver_plus')
+    os.system('pipenv run python manage.py runserver_plus')
 elif sys.argv[-1] == 'sh':
-    os.system('pipenv run python ./unolog/manage.py shell_plus')
+    os.system('pipenv run python manage.py shell_plus')
 elif sys.argv[-1] == 'm':
-    os.system('pipenv run python ./unolog/manage.py migrate')
+    os.system('pipenv run python manage.py migrate')
 elif sys.argv[-1] == 'mk':
-    os.system('pipenv run python ./unolog/manage.py makemigrations')
+    os.system('pipenv run python manage.py makemigrations')
 elif sys.argv[-1] == 'mm':
     os.system(
-        'pipenv run python ./unolog/manage.py makemigrations && pipenv run python ./unolog/manage.py migrate'
+        'pipenv run python manage.py makemigrations && pipenv run python ./unolog/manage.py migrate'
     )
 
 elif len(sys.argv) > 1:
-    os.system('pipenv run python ./unolog/manage.py {0}'.format(sys.argv[-1]))
+    os.system('pipenv run python manage.py {0}'.format(sys.argv[-1]))
